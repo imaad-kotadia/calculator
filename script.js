b34fe95985374e925e46f2d5484460eaf4931c4e
imaad-kotadia <imaadkotadia@gmail.com>
2026-04-28 23:08:08 +0400

    Added operator functionality
    Chaining operations does not work

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ const divide = (a, b) => a / b;
 
 let operands = ["", ""];
 let operator = "";
+let pastAnswer = false;
 
 const display = document.querySelector(".display");
 
 function clear() {
     operands = ["", ""];
     operator = "";
+    pastAnswer = false;
     updateDisplay();
 }
 function changeOperands(value) {
+    if (pastAnswer) {
+        clear();
+    }
     if (operands.at(0) === "") { //case 1: operator 1 is empty
         operands[0] += value;
     }
@@ -26,13 +31,60 @@ function changeOperands(value) {
     updateDisplay();
 }
 function changeOperator(newOperator) {
-    if (operands.at(0) != "") {
-        operator = newOperator;
+    if (pastAnswer) {
+        pastAnswer = false;
+    }
+    if (operands.at(0) != "") { 
+        if (operands.at(1) === "") {
+            operator = newOperator;
+        }
+        else {
+            operate();
+            operator = newOperator;
+        }
+        
+
     }
     updateDisplay();
 }
+function isOperationValid() {
+    if (operands.at(0) === "" || operands.at(1) === "") {
+        return false;
+    } 
+    if (operator === "/" && operands.at(1) === "0") {
+        alert("i know what you are.");
+        return false;
+    }
+    return true;
+}
 function operate() { 
-
+    if (isOperationValid()) {
+        let answer = 0;
+        let a = Number(operands.at(0));
+        let b = Number(operands.at(1));
+        switch (operator) {
+            case "+":
+                answer = add(a, b);
+                break;
+            case "-":
+                answer = subtract(a, b);
+                break;
+            case "/":
+                answer = divide(a, b);
+                break;
+            case "x":
+                answer = multiply(a, b);
+                break;
+        }
+        answer = String(Number(answer.toFixed(5)));
+        operands = [answer, ""];
+        operator = "";
+        pastAnswer = true;
+        updateDisplay();
+    }
+    else {
+        console.log("INVALID OPERATION");
+    }
 }
 function updateDisplay() {
     let res = ""
@@ -46,7 +98,29 @@ function updateDisplay() {
         res += operands.at(1);
     }
     console.log(res);
+    checkDecimal();
     display.textContent = res;
+}
+function addDecimal() {
+    if (!checkDecimal()) {
+        changeOperands(".");
+    }
+}
+function checkDecimal() {
+    if (operands.at(1) === "") { //case 1: working on first number
+        if (operands.at(0).includes(".")) {
+            decimal.disabled = true;
+            return true
+        }
+    }
+    else {
+        if (operands.at(1).includes(".")) {
+            decimal.disabled = true;
+            return true;
+        }
+    }
+    decimal.disabled = false;
+    return false;
 }
 
 
@@ -68,3 +142,11 @@ symbol.forEach(button => {
         changeOperator(button.textContent);
     });
 });
+
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener('click', addDecimal);
+
+const clearBtn = document.querySelector(".clear");
+if (clearBtn) {
+    clearBtn.addEventListener('click', clear);
+}
